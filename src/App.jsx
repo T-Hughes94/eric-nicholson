d72b75303994cd5e4871786c8e99d7e0a06a414c
Terence Hughes <Terencehughes450@gmail.com>
2024-05-14 16:35:29 -0400

    finished main component and wired to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './index.css';
+import Main from './components/Main'
 
 function App() {
   
 
   return (
     <>
-     
+     <Main/>
     </>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
+import coverPic from '../assets/eric-cover.png';
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div id="main" className="relative w-full h-screen">
+      {/* Background Image */}
+      <img
+        className="w-full h-full object-cover object-left scale-x-[-1]"
+        src={coverPic}
+        alt="Background image"
+      />
+
+      {/* Content Overlay */}
+      <div className="absolute top-0 left-0 w-full h-full bg-white/50 flex justify-center items-center">
+        <div className="max-w-[700px] text-center">
+
+         {/* Name */}
+          <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>Eric Nicholson</h1>
+
+          {/* Type Animation */}
+          <h2 className='justify-center flex sm:text-3xl text-2xl pt-4 text-gray-800'>
+            <TypeAnimation
+              sequence={[
+                'Account Executive',
+                1000,
+                'Father',
+                1000,
+                'Salesman',
+                1000,
+                'Professional',
+                1000,
+              ]}
+              wrapper="div"
+              cursor={true}
+              style={{ fontSize: '1em', paddingLeft: '5px' }}
+              repeat={Infinity}
+            />
+          </h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
